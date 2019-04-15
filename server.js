b1ad50509eece037eaa414f2c95c9a8e824d3301
stizzle123 <arifayanidowu2@yahoo.com>
2019-04-15 07:15:42 +0100

    Init commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+
+const app = express();
+
+app.use(fileUpload());
+
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    res.status(400).json({ msg: "No File uploaded" });
+  }
+  const file = req.files.file;
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`[Server]: Listening on Port ${port}`));
